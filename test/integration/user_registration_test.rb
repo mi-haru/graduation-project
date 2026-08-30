d680decc5966a6e52f32e891dd72926cd8a9a8fc
miharu sasaki <miharu_0201_0222@yahoo.co.jp>
2026-08-31 01:55:01 +0900

    Add user registration with Devise

---
+++ test/integration/user_registration_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UserRegistrationTest < ActionDispatch::IntegrationTest
+  test "user can sign up with valid information" do
+    assert_difference("User.count", 1) do
+      post user_registration_path, params: {
+        user: {
+          name: "テストユーザー",
+          email: "new-user@example.com",
+          password: "password123",
+          password_confirmation: "password123"
+        }
+      }
+    end
+
+    assert_equal "テストユーザー", User.last.name
+    assert_redirected_to root_path
+  end
+end
