ca05c2121828a1402e71d28900561f0f78f15845
miharu sasaki <miharu_0201_0222@yahoo.co.jp>
2026-08-31 23:22:48 +0900

    Add user login page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -9,6 +9,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "h1", text: /ひとつの巣に。/
     assert_select "img[alt='medi巣のロゴ']"
     assert_select "a[href=?]", new_user_registration_path, text: "新規登録"
-    assert_select "button", text: "ログイン"
+    assert_select "a[href=?]", new_user_session_path, text: "ログイン"
   end
 end
