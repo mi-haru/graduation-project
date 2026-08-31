29e7081386296e68e2c1975cf9b216976867f433
miharu sasaki <miharu_0201_0222@yahoo.co.jp>
2026-09-01 01:09:36 +0900

    Add user logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,8 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(_resource)
     home_path
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
