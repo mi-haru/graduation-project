f8287d2ead25ff9907bf6cc167d2c14a5af4f6b7
miharu sasaki <miharu_0201_0222@yahoo.co.jp>
2026-09-06 22:01:43 +0900

    服薬情報の詳細・編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :medications, only: %i[index show new create edit]
+  resources :medications, only: %i[index show new create edit update]
 
   get "home", to: "home#index", as: :home
   devise_for :users
